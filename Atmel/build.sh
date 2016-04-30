24e63e6bcdf28a685b816ed7564ed4458bccbea3
Blaise Gassend <gassend@google.com>
2016-04-29 21:33:16 -0700

    Hacked the code so that it starts up without any input signal. Still need to adjust the speed.

---
+++ Atmel/build.sh
@@ -0,0 +1,2 @@
+wine /home/gassend/temp/avr/avrasm2.exe -D BLUESERIES_12A_MULTI -fI -o BLUESERIES_12A_MULTI.hex BLHeli.asm -e BLUESERIES_12A_MULTI.eep
+wine /home/gassend/temp/avr/avrasm2.exe -D MULTISTAR_20Av2_MULTI -fI -o MULTISTAR_20Av2_MULTI .hex BLHeli.asm -e MULTISTAR_20Av2_MULTI.eep
